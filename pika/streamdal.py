9c46c113239f06717f0d6772962a915f289513a9
Mark G <mark@gregan.org>
2024-03-19 13:59:59 -0400

    Cleanup and fixes

--- pika/streamdal.py
+++ pika/streamdal.py
@@ -1,11 +1,16 @@
 # This file contains code for shimming this library with the Streamdal python SDK
-import os, logging
+import logging
+import os
+import threading
 from dataclasses import dataclass
-from streamdal import Audience, StreamdalConfig, StreamdalClient, ProcessRequest, ProcessResponse, \
-    OPERATION_TYPE_CONSUMER, OPERATION_TYPE_PRODUCER, EXEC_STATUS_ERROR
+from streamdal import (Audience, StreamdalConfig, StreamdalClient, ProcessRequest,
+                       EXEC_STATUS_ERROR, OPERATION_TYPE_PRODUCER, OPERATION_TYPE_CONSUMER)
+
+__all__ = ("StreamdalRuntimeConfig", "Audience", "streamdal_setup",
+           "streamdal_process", "OPERATION_TYPE_PRODUCER", "OPERATION_TYPE_CONSUMER")
 
 STREAMDAL_ENV_ADDRESS = "STREAMDAL_ADDRESS"
-STREAMDAL_ENV_AUTH_TOKEN   = "STREAMDAL_AUTH_TOKEN"
+STREAMDAL_ENV_AUTH_TOKEN = "STREAMDAL_AUTH_TOKEN"
 STREAMDAL_ENV_SERVICE_NAME = "STREAMDAL_SERVICE_NAME"
 
 STREAMDAL_DEFAULT_COMPONENT_NAME = "rabbitmq"
@@ -13,13 +18,14 @@ STREAMDAL_DEFAULT_OPERATION_NAME = "consume"
 
 LOGGER = logging.getLogger(__name__)
 
+
 @dataclass
 class StreamdalRuntimeConfig:
     audience: Audience
     strict_errors: bool = False
 
+
 def streamdal_setup():
-    print("STREAMDAL SETUP CALLED")
     address = os.getenv("STREAMDAL_ADDRESS")
     if address is None:
         raise ValueError(f"Missing required environment variable STREAMDAL_ADDRESS")
@@ -30,17 +36,20 @@ def streamdal_setup():
 
     service_name = os.getenv("STREAMDAL_SERVICE_NAME")
     if service_name is None:
-        service_name = "pika"
+        raise ValueError(f"Missing required environment variable STREAMDAL_SERVICE_NAME")
 
     return StreamdalClient(
         StreamdalConfig(
             streamdal_url=address,
             streamdal_token=auth_token,
-            service_name=service_name
+            service_name=service_name,
+            exit=threading.Event(),
         )
     )
 
-def streamdal_process(streamdal_client: StreamdalClient, operation_type: int, exchange_name: str, routing_key: str, msg: bytes, cfg: StreamdalRuntimeConfig) -> bytes:
+
+def streamdal_process(streamdal_client: StreamdalClient, operation_type: int, exchange_name: str, routing_key: str,
+                      msg: bytes, cfg: StreamdalRuntimeConfig) -> bytes:
     if streamdal_client is None:
         return msg
 
@@ -57,7 +66,7 @@ def streamdal_process(streamdal_client: StreamdalClient, operation_type: int, ex
 
     if resp.status == EXEC_STATUS_ERROR:
         # Check if we have an optional custom config passed to basic_publish()
-        if len(cfg) > 0 and cfg[0].strict_errors:
+        if isinstance(cfg, StreamdalRuntimeConfig) and cfg.strict_errors:
             # Strict errors specified, raise an exception
             raise Exception(f"Streamdal encountered an error processing a message: {resp.message}")
         else:
@@ -70,11 +79,12 @@ def streamdal_process(streamdal_client: StreamdalClient, operation_type: int, ex
     return resp.data
 
 
-def streamdal_generate_audience(operation_type: int, exchange_name: str, routing_key: str, cfg: StreamdalRuntimeConfig) -> Audience:
+def streamdal_generate_audience(operation_type: int, exchange_name: str, routing_key: str,
+                                cfg: StreamdalRuntimeConfig) -> Audience:
     component_name = STREAMDAL_DEFAULT_COMPONENT_NAME
     operation_name = STREAMDAL_DEFAULT_OPERATION_NAME
 
-    if exchange_name !="":
+    if exchange_name != "":
         operation_name = exchange_name
 
     if routing_key != "":
@@ -83,7 +93,6 @@ def streamdal_generate_audience(operation_type: int, exchange_name: str, routing
     if cfg is not None:
         component_name = cfg.audience.component_name
         operation_name = cfg.audience.operation_name
-
 
     return Audience(
         operation_type=operation_type,
